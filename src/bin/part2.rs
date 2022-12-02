e593f1cc9c6ee53c6f30f012a7f0dc5102a070a5
Gabriel Martins Silva <91923731+gabrielmsoptimalex@users.noreply.github.com>
2022-12-01 22:13:42 -0300

    day 1 - part 2

---
+++ src/bin/part2.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn main() {
+    let file = File::open("assets/day1/input2.txt").expect("Could not open file");
+    let reader = BufReader::new(file);
+
+    let mut sum = 0;
+
+    let mut elfs: Vec<u32> = vec![];
+
+    for line in reader.lines() {
+        let value = line.unwrap();
+
+        if value == "" {
+            elfs.push(sum);
+            sum = 0;
+        } else {
+            sum += value.parse::<u32>().unwrap();
+        }
+    }
+
+    elfs.push(sum);
+
+    let mut biggest = 0;
+    elfs.sort_by(|a, b| b.cmp(a));
+
+    for i in  0..3{
+        biggest += elfs[i];
+    }
+
+    println!("{}", biggest);
+}
